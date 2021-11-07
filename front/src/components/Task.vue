e252e3971455d138ff7236c6da31683f85d84700
kamekuramaisuke <taisukeoono350@gmail.com>
2021-11-07 17:38:29 +0900

    add taskpage and getAllTask

---
+++ front/src/components/Task.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+const tasks = ref([])
+
+async function getTasks(){
+  try{
+    const data = await fetch('http://localhost:8000/tasks');
+    tasks.value = await data.json()
+  } catch(err){
+    console.error('🔥', err)
+  }
+}
+
+(async () => {
+  await getTasks()
+})()
+
+</script>
+
+<template>
+  <div v-for="task in tasks" :key="task">
+    {{ task }}
+  </div>
+</template>
--- front/src/route/index.js
+++ front/src/route/index.js
@@ -1,22 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-/*
 import Home from '../components/Home.vue'
 import Task from '../components/Task.vue'
-*/
 
-/*
 const routes = [
   {
     path: '/',
     component: Home,
   },
   {
-    path: '/Task',
+    path: '/tasks',
     component: Task,
-  },
+  }
 ]
-*/
 
 const router = createRouter({
   history: createWebHistory(),

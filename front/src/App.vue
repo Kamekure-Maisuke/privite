f4673a292a19d82c4dc40528c8a9ab5dfc9db010
kamekuramaisuke <taisukeoono350@gmail.com>
2021-11-07 17:16:21 +0900

    router設定

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <router-view />
 </template>
 
 <style>
